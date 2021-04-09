4c8e483b1580471803fd03db0acd6ea1a4a7d849
phycog <guyth1997v2@gmail.com>
2021-04-09 17:16:52 +0700

    change text to guy thongtrachoo

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>My name is guy</p>
+        <p>My name is guy thongtrachoo</p>
       </header>
     </div>
   );
